1a57aafdef3875c186fb867f292b327c1faea15f
DiegP24 <diego_a_pulido@hotmail.com>
2022-09-20 19:48:02 -0500

    Inicio Mercado Liebre

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.listen(process.env.PORT || 3000)
+
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', (req, res) => { 
+   res.sendFile(path.resolve(__dirname, './src/views/home.html'));
+   
+});
+
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
